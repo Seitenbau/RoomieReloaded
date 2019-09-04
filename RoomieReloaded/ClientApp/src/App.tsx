49a469453727e8876f868b1e4cc6e506165f3b59
Tobias Häusler <tobias.haeusler@seitenbau.com>
2019-09-04 15:19:56 +0200

    load icon fonts from own server instead of "the cloud"

--- RoomieReloaded/ClientApp/src/App.tsx
+++ RoomieReloaded/ClientApp/src/App.tsx
@@ -4,7 +4,9 @@ import Calendar from './components/calendarView';
 import Navigation from './components/navigation';
 import { initializeIcons } from '@uifabric/icons';
 
-initializeIcons();
+//load icon fonts from local directory. if new icons should be used, it may be required to copy the fonts from /node_modules/@uifabric/icons/fonts to /public/icons/fonts
+// maybe add a build step to copy fonts automatically
+initializeIcons('./icons/fonts/');
 
 const App: React.FC = () => {
   return (
