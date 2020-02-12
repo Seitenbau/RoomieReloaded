62dd4d65dff24e2008a7e1e39f5ba18e2fd37bb1
Tobias Häusler <tobias.haeusler@seitenbau.com>
2020-02-12 16:41:58 +0100

    LDAP Client * migrate to Novell Ldap Client

--- RoomieReloaded/Configuration/LdapConfiguration.cs
+++ RoomieReloaded/Configuration/LdapConfiguration.cs
@@ -1,36 +1,37 @@
-﻿using System.DirectoryServices.Protocols;
-using System.Net;
+﻿using System;
 
 namespace RoomieReloaded.Configuration
 {
     public class LdapConfiguration
     {
-        public string Domain { get; set; }
+        private TimeSpan _timeout;
+
+        public string Host { get; set; }
+
+        public ushort Port { get; set; }
 
         public string UserName { get; set; }
 
         public string Password { get; set; }
 
-        public int TimeoutInMilliseconds { get; set; } = 3000;
+        public int TimeoutInMilliseconds
+        {
+            get => _timeout.Milliseconds;
+            set => _timeout = TimeSpan.FromMilliseconds(value);
+        }
+
+        public int TimeoutInSeconds => (int) _timeout.TotalSeconds;
 
         public string SearchBase { get; set; }
 
-        public NetworkCredential CreateCredentials()
-        {
-            return new NetworkCredential(
-                UserName,
-                Password,
-                Domain);
-        }
-
-        public LdapDirectoryIdentifier CreateIdentifier()
-        {
-            return new LdapDirectoryIdentifier(Domain);
-        }
-
         public bool Validate()
         {
-            if (string.IsNullOrEmpty(Domain))
+            if (string.IsNullOrEmpty(Host))
+            {
+                return false;
+            }
+
+            if (Port == 0)
             {
                 return false;
             }
