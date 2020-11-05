ee15f0c8693c84d78958ec70a925063bb0238a01
MaxAtoms <7847075+MaxAtoms@users.noreply.github.com>
2020-11-05 13:17:50 +0100

    Added getters for the date and calendar parameters

--- RoomieReloaded/ClientApp/src/services/parameter/parameterService.tsx
+++ RoomieReloaded/ClientApp/src/services/parameter/parameterService.tsx
@@ -1,4 +1,4 @@
-import { Moment } from "moment"
+import moment, { Moment } from "moment"
 import { CalendarType } from "../../reducers/calendarReducer"
 import { IQueryService, QueryService } from "../query/queryService";
 
@@ -14,6 +14,39 @@ export class ParameterService implements IParameterService
 {
     dateParamName:string = "viewDate";
     calendarParamName:string = "viewCalendar";
+
+    getDateTime = () : Moment | null => {
+        const dateParam = queryService.getUrlParam(this.dateParamName);
+
+        if(dateParam == null) {
+            return null;
+        }
+
+        const dateMoment = moment(dateParam);
+        return dateMoment.isValid()
+            ? dateMoment
+            : null;
+    }
+
+    getCalendar = () : CalendarType | null => {
+        const calendarParam = queryService.getUrlParam(this.calendarParamName);
+
+        if(calendarParam == null){
+            return null;
+        }
+
+        const upperCaseCalendarParam = calendarParam.toUpperCase();
+
+        switch(upperCaseCalendarParam){
+            case 'DAY':
+            case 'WEEK':
+            case 'MONTH':
+            case 'WORKWEEK':
+                return upperCaseCalendarParam;
+            default:
+                return null;
+        }
+    }
 
     setDateTime = (dateTime:Moment)  => {
         const formattedDate = dateTime.format("YYYY-MM-DD");
